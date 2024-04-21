a9793cd0699007057bf6a2307261531d04b85841
keiyonekawa0614 <kei.yonekawa0614@gmail.com>
2024-04-21 11:34:53 +0900

    create vpc

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,8 +1,16 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+// EC2に関するパッケージをimport
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // vpcを宣言
+    const vpc = new ec2.Vpc(this, "BlogVpc", {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+    })
+
   }
 }
