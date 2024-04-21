8696f3ee6c7078195c9db619324621e290c6e310
keiyonekawa0614 <kei.yonekawa0614@gmail.com>
2024-04-21 11:49:01 +0900

    create ec2

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -12,5 +12,16 @@ export class CdkWorkshopStack extends Stack {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
     })
 
+    // EC2インスタンスの宣言を準備
+    const webServer1 = new ec2.Instance(this, "WordpressServer1", {
+      // EC2インスタンスを起動するVPCを設定
+      vpc,
+      // t3.smallインスタンスタイプを設定
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      // AmazonLinuxImageインスタンスを生成し、AMIを設定
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+      }),
+    });
   }
 }
